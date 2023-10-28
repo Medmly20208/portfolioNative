092bd2486e1336a5734b4b7604d05835c62d7d2b
Mohamed Moulay <moulaymohamed856@gmail.com>
2023-10-28 21:15:33 +0100

    imporve content and performance

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const navIcon = document.getElementsByClassName("nav_icon")
 const mobileNav = document.getElementsByClassName("mobile_nav")
 const backdrop = document.getElementsByClassName("backdrop")
+const downlaodBtn = document.getElementById("download_resume")
+const fileLinks = document.getElementsByClassName("file_download")
 
 navIcon[0].onclick = ()=>{
     mobileNav[0].style.display="block"
@@ -8,4 +10,10 @@ navIcon[0].onclick = ()=>{
 
 backdrop[0].onclick = ()=>{
     mobileNav[0].style.display="none"
+}
+
+downlaodBtn.onclick =()=>{
+    fileLinks[0].click()
+   
+
 }
