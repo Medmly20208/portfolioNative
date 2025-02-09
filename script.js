98e28d7c6122cdc3376e2d2ec6f7ad2d1b076dba
Mohamed Moulay <moulaymohamed856@gmail.com>
2025-02-09 15:13:13 +0100

    delete personnel information

--- script.js
+++ script.js
@@ -1,46 +1,39 @@
-const navIcon = document.getElementsByClassName("nav_icon")
-const mobileNav = document.getElementsByClassName("mobile_nav")
-const mobileNavContent = document.getElementsByClassName("mobile_nav_content")
-const closeNavBtn = document.getElementsByClassName("close_icon_btn")
-const lists = document.getElementsByClassName("section_link mobile")
-const downlaodBtn = document.getElementById("download_resume")
-const header = document.getElementsByTagName("header")
-const fileLinks = document.getElementsByClassName("file_download")
+const navIcon = document.getElementsByClassName("nav_icon");
+const mobileNav = document.getElementsByClassName("mobile_nav");
+const mobileNavContent = document.getElementsByClassName("mobile_nav_content");
+const closeNavBtn = document.getElementsByClassName("close_icon_btn");
+const lists = document.getElementsByClassName("section_link mobile");
+const downlaodBtn = document.getElementById("download_resume");
+const header = document.getElementsByTagName("header");
+const fileLinks = document.getElementsByClassName("file_download");
+const currentYearWrapper = document.getElementById("currentYear");
+const currentYear = new Date().getFullYear();
 
+currentYearWrapper.innerText = currentYear;
 
+navIcon[0].onclick = () => {
+  mobileNavContent[0].style.left = "0px";
+};
 
+window.onscroll = () => {
+  if (window.scrollY != 0) {
+    return header[0].classList.add("header_shadow");
+  }
+  header[0].classList.remove("header_shadow");
+};
 
-navIcon[0].onclick = ()=>{
-    mobileNavContent[0].style.left = "0px"
-  
-}
+console.log(typeof lists);
+Object.values(lists).map((list) => {
+  list.onclick = () => {
+    mobileNavContent[0].style.left = "-100vw";
+  };
+  return;
+});
 
-window.onscroll = ()=>{
-    
-    if(window.scrollY!=0){
-        return header[0].classList.add("header_shadow")
-    }
-    header[0].classList.remove("header_shadow")
-}
+closeNavBtn[0].onclick = () => {
+  mobileNavContent[0].style.left = "-100vw";
+};
 
-console.log(typeof(lists))
-Object.values(lists).map((list)=>{
-   
-     list.onclick = ()=>{
-        mobileNavContent[0].style.left = "-100vw"
-     }
-    return 
-})
-
-closeNavBtn[0].onclick = ()=>{
-    
-    mobileNavContent[0].style.left = "-100vw"
-}
-
-
-
-downlaodBtn.onclick =()=>{
-    fileLinks[0].click()
-   
-
-}
+downlaodBtn.onclick = () => {
+  fileLinks[0].click();
+};
